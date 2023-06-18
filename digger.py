9933124636a34fa896034ed010bc4c4edecb652c
Vntoine <antoine@mouezant.fr>
2023-06-18 14:34:17 +0200

    Amélioration - ThreadPoolExecutor

--- digger.py
+++ digger.py
@@ -1,15 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import exrex
+import concurrent.futures
+import time
 
-tentatives = 1000
-tab = [i for i in range(50,1000,50)]
-
-for i in range(tentatives):
-    code = ''.join(exrex.getone('[A-Za-z0-9_\-]') for loop in range(11))
-    retour = BeautifulSoup(requests.get('https://www.youtube.com/watch?v='+code).content,'html.parser')
-    res = str(retour.find('title'))
+def dig(code : str):
+    res = str(BeautifulSoup(requests.get('https://www.youtube.com/watch?v='+code,timeout=3).content,'html.parser').find('title'))
     if res != "<title> - YouTube</title>":
-        print("Trouvé : "+res+" -----> "+code)
-    if i in tab:
-        print('In progress... ',str(i//10)+"%")
+        return "Trouvé : "+res+" -----> "+code
+
+tentatives = 100
+thread_start = time.time()
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    futures = []
+    for i in range(tentatives):
+        code = ''.join(exrex.getone('[A-Za-z0-9_\-]') for loop in range(11))
+        futures.append(executor.submit(dig,code))
+    for future in concurrent.futures.as_completed(futures):
+        if future.result() is not None:
+            print(future.result())
+print('\nOVER ⛏️  :',str(time.time() - thread_start)[:4],"secondes\n")
